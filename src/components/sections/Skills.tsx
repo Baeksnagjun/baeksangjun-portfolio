cd30b2f4c4d8e3a68d37e70b048bafa7168b4cbf
Baeksnagjun <juny200@naver.com>
2026-09-03 14:36:59 +0900

    스킬골조완성

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -2,5 +2,33 @@ import { skills } from '../../data/skills';
 import styles from './Skills.module.css';
 
 export default function Skills() {
-  return <section className={styles.skills} data-count={skills.length} />;
+  return (
+    <section className={styles.skills}>
+      <h2 className={styles.title}>📎Skills</h2>
+
+      <div className={styles.grid}>
+
+
+          <div className={styles.cards}>
+            <div className={styles.card}>
+              <p className={styles.label}>Languages</p>
+              <p className={styles.value}></p>
+            </div>
+            <div className={styles.card}>
+              <p className={styles.label}>Framework & Libraries</p>
+              <p className={styles.value}></p>
+            </div>
+            <div className={styles.card}>
+              <p className={styles.label}>Tools</p>
+              <p className={styles.value}></p>
+            </div>
+            
+
+
+          
+        </div>
+      </div>
+    </section>
+  )
+  //<section className={styles.skills} data-count={skills.length} />;
 }
