a240bb1dad736db205bbcb6f0bad885736f80c90
Baeksnagjun <juny200@naver.com>
2026-09-03 10:20:27 +0900

    헤더 완료

--- src/components/common/Gnb.tsx
+++ src/components/common/Gnb.tsx
@@ -4,7 +4,14 @@ import styles from './Gnb.module.css';
 export default function Gnb() {
   return (
     <header className={styles.gnb}>
+      <h2 className={styles.logo}>백상준 포트폴리오</h2>
+      <nav className={styles.menu}>
+      <h3 className={styles.pageBtn}>About me</h3>
+      <h3 className={styles.pageBtn}>Skills</h3>
+      <h3 className={styles.pageBtn}>Projects</h3>
+      <h3 className={styles.pageBtn}>Career</h3>
       <ThemeSwitcher />
+      </nav>
     </header>
   );
 }
