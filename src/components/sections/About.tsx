f60c8a6da6c82e1e0ed0adeebff5a7128ee41f7a
Baeksnagjun <juny200@naver.com>
2026-09-05 20:36:48 +0900

    어바웃미 탄스테구커리 작성 완료

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,69 +1,57 @@
 
+import { useQuery } from '@tanstack/react-query';
 import styles from './About.module.css';
+import { supabase } from '../../shared/api/supabase';
 
 export default function About() {
   
-  type Profile = {
-  name: string;
-  tagline: string;
-  summary: string;
-  lastUpdate: string;
-  photo: string;
-  birthdate: string;
-  location: string;
-  education: string;
-  about: string;
-  email: string;
-  github: string;
-  resumeUrl: string;
-};
 
-const profile: Profile = {
-  name: '백상준',
-  tagline: '안녕하세요 개발자',
-  summary: '백엔드 개발자를 희망하고 있습니다',
-  lastUpdate: '2026년',
-  photo: '/profilleph.jpeg',
-  birthdate: '2000년 2월 10일',
-  location: '서울시 서대문구',
-  education: '한성대학교 컴퓨터공학과',
-  about: '자기소개입니다자기소개입니다자기소개입니다자기소개입니다자기소개입니다자기소개입니다자기소개입니다자기소개입니다자기소개입니다자기소개입니다자기소개입니다자기소개입니다자기소개입니다',
-  email: 'juny200@naver.com',
-  github: '깃허브@주소',
-  resumeUrl: '',
-};
+
+const {data, isPending, error}=useQuery({
+  queryKey:['profile'],
+  queryFn: async ()=>{
+    const { data, error}= await supabase.from('profile').select('*').single();
+    if (error) throw error;
+    return data;
+  },
+})
+if (isPending) return <section id="about">불러오는 중</section>;
+if (error) return <section id="about">불러오기 실패</section>;
+
+
+
 
   return (
     <section id="about" className={styles.about}>
       <h2 className={styles.title}>📎ABOUT ME</h2>
 
       <div className={styles.grid}>
-        <img className={styles.photo} src={profile.photo} alt="프로필" />
+        <img className={styles.photo} src={data.photo} alt="프로필" />
 
         <div className={styles.right}>
           <div className={styles.cards}>
             <div className={styles.card}>
               <p className={styles.label}>이름</p>
-              <p className={styles.value}>{profile.name}</p>
+              <p className={styles.value}>{data.name}</p>
             </div>
             <div className={styles.card}>
               <p className={styles.label}>생년월일</p>
-              <p className={styles.value}>{profile.birthdate}</p>
+              <p className={styles.value}>{data.birthdate}</p>
             </div>
             <div className={styles.card}>
               <p className={styles.label}>위치</p>
-              <p className={styles.value}>{profile.location}</p>
+              <p className={styles.value}>{data.location}</p>
             </div>
             <div className={styles.card}>
               <p className={styles.label}>학력</p>
-              <p className={styles.value}>{profile.education}</p>
+              <p className={styles.value}>{data.education}</p>
             </div>
           </div>
 
           
         </div>
       </div>
-      <p className={styles.desc}>{profile.about}</p>
+      <p className={styles.desc}>{data.about}</p>
     </section>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDom from 'react-dom/client'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const queryclient = new QueryClient();
+
+ReactDom.createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryclient}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </QueryClientProvider>
+
 )
